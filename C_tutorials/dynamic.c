4d075aa95ac1a5798674f9668aa053dfb672917f
Seongwoo Choi <swchoi1994@gmail.com>
2020-06-19 00:53:20 +0900

    Adding dynamic memory allocation

--- C_tutorials/dynamic.c
+++ C_tutorials/dynamic.c
@@ -1,5 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+/* 
+the simplest standard library function that allocates memory at runtime
+is called malloc()
+- need to include the stdlib.h header file
+- you specify the number of bytes of memory that you want allocated
+as the argument
+- returns the address of the first byte of memory that is allocated
+- because you get an address returned, a pointer is the only place to 
+put it
+
+int *pNumber = (int*)malloc(100);
+
+-------------------------------------------------------------------
+the calloc() function offers a couple of advantages over malloc()
+- it allocates memory as a number of elements of a give size
+- it initializes the memory that is allocated so that all bytes are 
+zero.
+
+
+calloc() function requires two argument values
+- number of data items for which space is required
+- size of each data item
+
+int *pNumber = (int*)calloc(75, sizeof(int));
+
+the return value wil be NULL if it was not possible to allocate the 
+memory requested
+- very similar to using malloc(), but the big plus is that you know the
+memory are will be initialized to 0 
+
+-------------------------------------------------------------------
+the realloc() function enables you to reuse or extend memory that you
+previosuly allocated using malloc() or calloc()
+
+expects two argument values
+- a pointer containing an address that was previously returned by a call to
+malloc(), calloc()
+- the size in bytes of the new memory that you want allocated
+
+allocates the amount of memory you specify by the second argument
+- transfers the contents of the previusly llocated memory referenced by 
+the pointer that you supply as the first argument to the newly allocated 
+memory
+- returns a void* pointer to the new memory or NULL if the operation fails
+for some reason. 
+
+
+*/ 
 
 int main() {
     int size;
