1fe7609e17c23eb9dd4d29d99ee4d57bcac8f2c6
Seongwoo Choi <swchoi1994@gmail.com>
2020-06-19 01:02:04 +0900

    Adding dynamic memory allocation

--- C_tutorials/dynamic.c
+++ C_tutorials/dynamic.c
@@ -98,4 +98,9 @@ release the memory
 always ensure that you provide for releasing memory that you have allocated
 -deicde where in your code you will release the memory when you write
 the code that allocates it. 
+
+make sure you do not inadvertently overwrite the address of memory 
+you have allocated on the heap before you have released it 
+- will cause a memory leak
+- be especially careful when allocating memory within a loop.
 */
